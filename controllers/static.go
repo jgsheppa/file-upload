006bae6d51fd35483d686c6932b98c8206eaae1a
jgsheppa <james.sheppard@avisio.ai>
2023-07-09 01:22:13 +0200

    clean up main.go file

---
+++ controllers/static.go
@@ -0,0 +1,100 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/getsentry/sentry-go"
+	"github.com/gin-gonic/gin"
+)
+
+func Ping(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
+func Redirect(c *gin.Context) {
+	c.Redirect(http.StatusMovedPermanently, "/httpRedirect2")
+}
+
+func RedirectDestination(c *gin.Context) {
+	c.JSON(200, gin.H{"hello": "world"})
+}
+
+func RouterRedirectDestination(c *gin.Context) {
+	c.JSON(200, gin.H{"hello": "world"})
+}
+
+func SentryError(c *gin.Context) {
+	sentry.CaptureMessage("An Error!")
+	c.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+}
+
+func SentryRecover(c *gin.Context) {
+	defer sentry.Recover()
+	panic("A Fatal Error!")
+}
+
+func Cookie(c *gin.Context) {
+
+	cookie, err := c.Cookie("gin_cookie")
+
+	if err != nil {
+		cookie = "NotSet"
+		c.SetCookie("gin_cookie", "test", 3600, "/", "localhost", false, true)
+	}
+	c.String(http.StatusOK, fmt.Sprintf("Here's your cookie's name: %s", cookie))
+}
+
+func QueryParams(c *gin.Context) {
+	firstname := c.DefaultQuery("firstname", "Guest")
+	lastname := c.Query("lastname") // shortcut for c.Request.URL.Query().Get("lastname")
+
+	c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
+}
+
+// Grouping routes example
+// Simple group: v1
+func V1(r *gin.Engine) {
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/login", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "login1",
+			})
+		})
+		v1.GET("/submit", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "submit1",
+			})
+		})
+		v1.GET("/read", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "read1",
+			})
+		})
+	}
+}
+
+// Simple group: v2
+func V2(r *gin.Engine) {
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/login", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "login2",
+			})
+		})
+		v2.GET("/submit", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "submit2",
+			})
+		})
+		v2.GET("/read", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "read2",
+			})
+		})
+	}
+}
--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/getsentry/sentry-go"
 	sentrygin "github.com/getsentry/sentry-go/gin"
 	"github.com/gin-gonic/gin"
+	"github.com/jgsheppa/gin-playground/controllers"
 	"github.com/jgsheppa/gin-playground/models"
 	"github.com/joho/godotenv"
 )
@@ -22,8 +23,6 @@ func init() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-}
-func main() {
 	sentryKey := os.Getenv("SENTRY_KEY")
 	environment := os.Getenv("ENVIRONMENT")
 	if err := sentry.Init(sentry.ClientOptions{
@@ -39,6 +38,8 @@ func main() {
 	}); err != nil {
 		fmt.Printf("Sentry initialization failed: %v\n", err)
 	}
+}
+func main() {
 
 	s := models.NewServices("dev.db")
 	err := s.AutoMigrate()
@@ -76,28 +77,18 @@ func main() {
 	})
 
 	// Basic ping example
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/ping", controllers.Ping)
 
 	// HTTP redirect example
-	r.GET("/httpRedirect", func(c *gin.Context) {
-		c.Redirect(http.StatusMovedPermanently, "/httpRedirect2")
-	})
-	r.GET("/httpRedirect2", func(c *gin.Context) {
-		c.JSON(200, gin.H{"hello": "world"})
-	})
+	r.GET("/httpRedirect", controllers.Redirect)
+	r.GET("/httpRedirect2", controllers.RedirectDestination)
 
 	// Router redirect example
 	r.GET("/routerRedirect", func(c *gin.Context) {
 		c.Request.URL.Path = "/routerRedirect2"
 		r.HandleContext(c)
 	})
-	r.GET("/routerRedirect2", func(c *gin.Context) {
-		c.JSON(200, gin.H{"hello": "world"})
-	})
+	r.GET("/routerRedirect2", controllers.RouterRedirectDestination)
 
 	// Upload file example. Use curl to test it.
 	//
@@ -179,82 +170,23 @@ func main() {
 	})
 
 	// Capture a Sentry error when there is a 500 error.
-	r.GET("/error", func(c *gin.Context) {
-		sentry.CaptureMessage("An Error!")
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
-	})
+	r.GET("/error", controllers.SentryError)
 
 	// Sentry recovers from panic errors
 	// and creates an issue, if a issues for
 	// fatal errors have been set.
-	r.GET("/fatal", func(c *gin.Context) {
-		defer sentry.Recover()
-		panic("A Fatal Error!")
-	})
+	r.GET("/fatal", controllers.SentryRecover)
 
 	// Cookie example
-	r.GET("/cookie", func(c *gin.Context) {
-
-		cookie, err := c.Cookie("gin_cookie")
-
-		if err != nil {
-			cookie = "NotSet"
-			c.SetCookie("gin_cookie", "test", 3600, "/", "localhost", false, true)
-		}
-
-		fmt.Printf("Cookie value: %s \n", cookie)
-	})
+	r.GET("/cookie", controllers.Cookie)
 
 	// Query string parameters are parsed using the existing underlying request object.
 	// The request responds to a url matching:  /welcome?firstname=Jane&lastname=Doe.
 	// Check out http://localhost:8080/welcome?firstname=Jane&lastname=Doe
-	r.GET("/welcome", func(c *gin.Context) {
-		firstname := c.DefaultQuery("firstname", "Guest")
-		lastname := c.Query("lastname") // shortcut for c.Request.URL.Query().Get("lastname")
+	r.GET("/welcome", controllers.QueryParams)
 
-		c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
-	})
-
-	// Grouping routes example
-	// Simple group: v1
-	v1 := r.Group("/v1")
-	{
-		v1.GET("/login", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"message": "login1",
-			})
-		})
-		v1.GET("/submit", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"message": "submit1",
-			})
-		})
-		v1.GET("/read", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"message": "read1",
-			})
-		})
-	}
-
-	// Simple group: v2
-	v2 := r.Group("/v2")
-	{
-		v2.GET("/login", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"message": "login2",
-			})
-		})
-		v2.GET("/submit", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"message": "submit2",
-			})
-		})
-		v2.GET("/read", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"message": "read2",
-			})
-		})
-	}
+	controllers.V1(r)
+	controllers.V2(r)
 
 	// Run the server per default on port 8080
 	r.Run()

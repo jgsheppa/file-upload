9d46975fb5f952553c3ecbe42a87746f2a6db22b
jgsheppa <james.sheppard@avisio.ai>
2023-07-17 00:00:39 +0200

    log error from failed env load

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatalf("Error loading .env file: %v\n", err)
 	}
 	sentryKey := os.Getenv("SENTRY_KEY")
 	environment := os.Getenv("ENVIRONMENT")
